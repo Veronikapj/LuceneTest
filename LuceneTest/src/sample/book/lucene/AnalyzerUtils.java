ab11a419bea4ef6091637ade13728235d00362cb
pilju <pilju@192.168.0.12>
2018-07-22 19:38:45 +0900

    책과 비

--- LuceneTest/src/sample/book/lucene/AnalyzerUtils.java
+++ LuceneTest/src/sample/book/lucene/AnalyzerUtils.java
@@ -6,6 +6,7 @@ import java.io.StringReader;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.tokenattributes.KeywordAttribute;
 
 public class AnalyzerUtils {
 
@@ -21,6 +22,15 @@ public class AnalyzerUtils {
 		}
 		System.out.println("");
 		stream.close();
+		
+		/**
+		 * 책에 있는 코드
+		 * TermAttribute term = stream.addAttribute(TermAttribute.clss);
+		 * 
+		 * TermAttribute를 현재 루씬에서 찾을 수 없어서 CharTermAttribute로 바꿔서 사용했다.
+		 * stream.reset()/close() 를 추가로 넣었다.
+		 */
+		
 	}
 	
 }
