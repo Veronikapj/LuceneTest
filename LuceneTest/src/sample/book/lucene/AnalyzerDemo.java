be43867163b7f9ccc82f52c7af869b103e9beb66
pilju <pilju@192.168.0.12>
2018-07-23 23:17:31 +0900

    StopAnalyzer 분석, 다른 Analyzer comments

--- LuceneTest/src/sample/book/lucene/AnalyzerDemo.java
+++ LuceneTest/src/sample/book/lucene/AnalyzerDemo.java
@@ -16,10 +16,17 @@ public class AnalyzerDemo {
 	};
 	
 	private static final Analyzer[] analyzers = new Analyzer[] {
+			// 공백 문자를 기준으로 토큰을 분리한다.
 			new WhitespaceAnalyzer(),
+			// 숫자나 공백, 기호등으로 토큰을 분리하고, 글자는 모두 소문자로 변경한다.
 			new SimpleAnalyzer(),
+			// 숫자나 공백, 기호 등으로 토큰을 분리하고, 글자는 모두 소문자로 변경한 후, 불용어를 제거한다.
+			// 불용어 : 너무 흔해 거의 모든 문서에 다 들어 있어서 문서를 구분하는 의미가 없다고 생각하는 단어.
 			new StopAnalyzer(),
+			// 문법을 기반으로 토큰을 분리하는 고급문석기다. 이메일, 약어, 한중일 글자, 일반 알파벳 글자 등을 인식하고
+			// 토큰으로 분리한다. 알파벳은 모두 소문자로 변경하고, 불용어도 제거한다.
 			new StandardAnalyzer()
+			// Keyword Analyzer : 필드의 전체 원문을 하나의 토큰으로 처리한다.
 	};
 	
 	public static void main(String[] args) throws IOException{
